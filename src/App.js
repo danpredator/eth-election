1d01ee433a81b217568e961021cdfad07c6a0257
danpredator <danpredator@users.noreply.github.com>
2020-07-09 01:45:57 +0530

    Fetching candidates votes and called vote function, voters address restriction pending

--- src/App.js
+++ src/App.js
@@ -1,8 +1,98 @@
-import React,{ Component }from 'react';
-import './App.css';
+import React,{ Component }from 'react'
+import './App.css'
 import Main from './Main.js'
 
+import Web3 from 'web3'
+import Election from './abis/Election.json'
+
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      console.log(window.web3)
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account : accounts[0]})
+
+    //  web3.eth.getCoinbase((err, account) => {
+    //   if (err === null) {
+    //     this.setState({ account : account})
+    //   }
+    // });
+
+    const networkId = await web3.eth.net.getId()
+    const networkData = Election.networks[networkId]
+    if(networkData) {
+      const election = web3.eth.Contract(Election.abi, networkData.address)
+      // console.log(election.methods.createRecord)
+      this.setState({ election })
+      
+      const candidatesCount = await election.methods.candidatesCount().call()
+      // console.log(candidatesCount.toString())
+      this.setState({ candidatesCount })
+      //Load records
+      for (var i = 1; i <= candidatesCount; i++) {
+        const candidate = await election.methods.candidates(i).call()
+        this.setState({
+          candidates: [...this.state.candidates, candidate]
+        })
+      }
+      this.setState({ loading: false})
+      // console.log(this.state.candidates)
+    } else {
+      window.alert('contract not deployed to any network.')
+    }
+
+
+
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account : '',
+      candidatesCount : 0,
+      candidates : [],      
+      loading : true
+    }
+
+    this.vote = this.vote.bind(this)
+  }
+
+  vote(id) {
+    this.setState({ loading: true })
+    // console.log(name, gen,dob,blood,all,height,weight,sug)
+    // (pname,"m","25-2-1909",'o','none',price,82,true)
+
+    this.state.election.methods.vote(id)
+      .send({ from: this.state.account })
+      .once('receipt', (receipt) => {
+        this.setState({ loading: false })
+    })
+  }
+
+
+
+
   render() {
   return (
     <div>
@@ -14,7 +104,7 @@ class App extends Component {
           </span>
           <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className="text-white"><span id="account">0xvdbfvi34</span></small>
+              <small className="text-white"><span id="account">{this.state.account}</span></small>
             </li>
           </ul>
       </nav>
@@ -26,12 +116,15 @@ class App extends Component {
           <h1 class="text-center">Election Results</h1>
           
            <main role="main" className="col-lg-12 d-flex mt-4">
-              { false
+              { this.state.loading
                 ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
                 : <Main
+                  candidates={this.state.candidates}
+                  vote={this.vote}
                   />
               }
             </main>
+            <p id="accountAddress" class="text-center">your accountAddress is {this.state.account}</p>
 
         </div>
       </div>
--- src/Main.js
+++ src/Main.js
@@ -15,32 +15,37 @@ class Main extends Component {
               <th scope="col">Votes</th>
             </tr>
           </thead>
-          <tr>
-            <td>#</td>
-            <td>Name</td>
-            <td>Votes</td>
-          </tr>
+          {this.props.candidates.map((candidate, key) => {
+                return(
+                  <tr key={key}>
+                    <th scope="row">{candidate.id.toString()}</th>
+                    <td>{candidate.name}</td>
+                    <td>{candidate.voteCount.toString()}</td>
+                  </tr>
+                )
+            })}
 
-          <tr>
-            <td>#</td>
-            <td>Name</td>
-            <td>Votes</td>
-          </tr>
         </table>
 
         <hr/>
-        <form onSubmit="" class="text-center">//App.castVote(); return false;
+        <form class="text-center" onSubmit={(event) => {
+                  event.preventDefault()
+                  
+                  this.props.vote(this.candi)
+                }}>
           <div class="form-group">
             <label for="candidatesSelect">Select Candidate : &nbsp;</label>
             <input id="candi" type="radio" name="candi"
-            required /> candi 1   &nbsp;
+              onChange={(input) => { this.candi = 1}}
+            required /> candidate 1   &nbsp;
             <input id="candi" type="radio" name="candi"
-            required /> candi 2
+              onChange={(input) => { this.candi = 2}}
+            required /> candidate 2
           </div>
           <button type="submit" class="btn btn-primary">Vote</button>
         </form>
 
-        <p id="accountAddress" class="text-center">your accountAddress is jhvdah</p>
+        
       </div>
     </div>
     
